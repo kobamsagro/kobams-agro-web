078fdd0b0eda01a76fbeb4f91894cf8192c6f63a
David Bradford <davidbradforduko@yahoo.com>
2025-11-21 17:31:06 +0100

    code fixes

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -92,6 +92,7 @@ export const seed = async ({
         name: 'Demo Author',
         email: 'demo-author@example.com',
         password: 'password',
+        roles: ['author'],
       },
       draft: false,
     }),
